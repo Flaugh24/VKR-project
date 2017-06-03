190bed082aa76b35beae00ba11ec29ab45ede34c
gagarkin <gagarkin.iiu@gmail.com>
2017-06-03 18:10:06 +0300

    correction CustomUser
    Signed-off-by: gagarkin <gagarkin.iiu@gmail.com>

--- project/src/main/java/org/barmaley/vkr/autentication/CustomUser.java
+++ project/src/main/java/org/barmaley/vkr/autentication/CustomUser.java
@@ -1,19 +1,13 @@
 package org.barmaley.vkr.autentication;
+import org.barmaley.vkr.domain.Users;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.util.List;
 
-public class CustomUser implements UserDetails {
+public class CustomUser extends Users implements UserDetails {
 
     private String username;
-    private Integer id;
     private String password;
-    private String phoneNumber;
-    private String email;
-    private String firstName;
-    private String secondName;
-    private String surname;
-
     /* Spring Security related fields*/
     private List<GrantedAuthority> authorities;
     private boolean accountNonExpired = true;
@@ -31,14 +25,6 @@ public class CustomUser implements UserDetails {
         this.username = username;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     @Override
     public String getPassword() {
         return password;
@@ -46,46 +32,6 @@ public class CustomUser implements UserDetails {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
     }
 
     @Override
